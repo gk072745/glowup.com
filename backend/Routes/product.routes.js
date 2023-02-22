bd8e5eab25d02c63dd06774678d22f02b8f2a3a3
G-Dharani-raj <gdharaniraj0@gmail.com>
2023-02-22 15:18:43 +0530

    fixed search routes

--- backend/Routes/product.routes.js
+++ backend/Routes/product.routes.js
@@ -73,7 +73,7 @@ productRouter.get("/", async (req, res) => {
 
 // * individual product route
 
-productRouter.get("/:id", async (req, res) => {
+productRouter.get("/product/:id", async (req, res) => {
 	const id = req.params.id;
 	try {
 		let product = await ProductModel.findById(id);
@@ -85,24 +85,74 @@ productRouter.get("/:id", async (req, res) => {
 
 // * search route
 productRouter.get("/search", async (req, res) => {
-	const { q } = req.query;
-	if (!q) {
-		res.send({ status: 400, message: "Please enter a valid search term" });
-	} else {
-		let tmp = {};
-		tmp = {
+	const { category, brand, sort, order, product_type, q } = req.query;
+	let { page, limit } = req.query;
+	if (page === undefined || page < 1) {
+		page = 1;
+	}
+	if (limit === undefined || limit < 1) {
+		limit = 10;
+	}
+	try {
+		let tmp = {
 			$or: [
 				{ name: { $regex: q, $options: "i" } },
 				{ brand: { $regex: q, $options: "i" } },
 				{ product_type: { $regex: q, $options: "i" } },
 			],
 		};
-		try {
-			let data = await ProductModel.find(tmp);
-			res.send({ status: 200, data: data });
-		} catch (error) {
-			res.send({ status: 400, error: error });
+		if (category && brand) {
+			tmp = {
+				...tmp,
+				$and: [
+					{
+						category: {
+							$regex: category,
+							$options: "i",
+						},
+					},
+					{
+						brand: {
+							$regex: brand,
+							$options: "i",
+						},
+					},
+				],
+			};
+		} else if (category) {
+			tmp = { ...tmp, category: { $regex: category, $options: "i" } };
+		} else if (brand) {
+			tmp = { ...tmp, brand: { $regex: brand, $options: "i" } };
 		}
+		if (product_type) {
+			tmp = {
+				...tmp,
+				product_type: { $regex: product_type, $options: "i" },
+			};
+		}
+		const totalPages = await ProductModel.find(tmp);
+		let data = [];
+		if (sort) {
+			data = await ProductModel.find(tmp)
+				.sort(
+					sort === "rating"
+						? { rating: order === "asc" ? 1 : -1 }
+						: { price: order === "asc" ? 1 : -1 }
+				)
+				.limit(limit)
+				.skip(limit * page);
+		} else {
+			data = await ProductModel.find(tmp)
+				.limit(limit)
+				.skip(limit * page);
+		}
+		res.send({
+			status: 200,
+			data: data,
+			totalPages: Math.ceil(totalPages.length / limit),
+		});
+	} catch (error) {
+		res.send({ status: 400, error: error });
 	}
 });
 module.exports = productRouter;
