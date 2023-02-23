4fa5c78df392f510f9bf3f5f5d09013cbb69d6ff
nikhildeora <fitnexy@gmail.com>
2023-02-23 22:17:14 +0530

    product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import {Route,Routes} from "react-router-dom"
-import AllPages from "./Pages/AllPages/AllPages"
+
 
 function App() {
   return (
     <div className="App">
-        <Routes >
-          <Route path='products/:category' element={<AllPages />} />
-        </Routes>
+       
     </div>
   );
 }
