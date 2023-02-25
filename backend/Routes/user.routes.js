62bbe3503c0281a2fd37b8a9945cf91b9bb5fb56
Dharani Raj <105777295+G-Dharani-raj@users.noreply.github.com>
2023-02-26 00:35:30 +0530

    Fw21 0279 day3 (#52)
    * initialized server
    
    * connected db
    
    * finished admin routes
    
    * added search and query search
    
    * added user and admin authentication middlewares
    
    * added cart
    
    * fixed an error in product route
    
    * added pagination for get routes
    
    * fixed search routes
    
    * added wishlist model
    
    * done with wishlist routes
    
    * adding status codes
    
    * added status codes
    
    * added proper status codes and shifted to cookie based auth
    
    * conflicts with magnify and magnifier fixed
    
    * added status codes in user middleware
    
    * fixed pagination
    
    * updates the ratings and categories
    
    * started working on sign in and sign up
    
    * stashing changes
    
    * finished login signup functionality
    
    * removed dupe lines
    
    * chnaged the secure settings
    
    * added exposed header to the cors
    
    * added lax
    
    * samesite => none
    
    * test
    
    * added domain

--- backend/Routes/user.routes.js
+++ backend/Routes/user.routes.js
@@ -40,6 +40,7 @@ userRouter.post("/login", async (req, res) => {
 						sameSite: "none",
 
 						secure: true,
+						domain: "http://localhost:3000",
 					});
 					res.status(200);
 					res.send({
