6b1cfaf88c87b64ec95ca62dab9f5765fd5edaa6
G-Dharani-raj <gdharaniraj0@gmail.com>
2023-06-26 18:54:37 +0530

    changed a type in the api requests

--- frontend/src/Homepage/navbar/button/buttonsignin.jsx
+++ frontend/src/Homepage/navbar/button/buttonsignin.jsx
@@ -13,83 +13,97 @@ import {
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
 
 const Buttonsign = () => {
+	const { isLoggedin } = useSelector((store) => store.userManager);
+	const dispatch = useDispatch();
 	return (
-		<div className="button">
-			<Popover>
-				<PopoverTrigger>
-					<Button
-						bg="none"
-						color="#fff"
-						fontWeight={500}
-						fontSize={15}
-					>
-						Signin
-					</Button>
-				</PopoverTrigger>
-
-				<PopoverContent
-					className="layout"
-					w={"100%"}
-					p={15}
-					mt={15}
-					zIndex={1}
-				>
-					<PopoverArrow />
-					<div className="popover">
-						<PopoverHeader fontSize={25}>
-							Login / Create Account
-						</PopoverHeader>
-						<PopoverCloseButton w={8} border="none" bg="none" />
-					</div>
-					<PopoverBody
-						textAlign={"left"}
-						w={280}
-						fontSize={15}
-						mt={6}
-						color="grey"
-					>
-						Register now and get 2000 Nykaa reward points instantly!
-					</PopoverBody>
-					<div className="signupbutt">
-						<div>
-							<Link className="link1" to="/login">
-								Sign in with Mobile/Email{" "}
-							</Link>
-						</div>
-						<div>
-							<i>
-								<BsArrowRight size={15} />
-							</i>
-						</div>
-					</div>
-					<div>
-						<Link className="link1" to="/signup">
-							Sign Up{" "}
-						</Link>
-					</div>
-					<div className="google">
-						<div>
-							<i>
-								<FcGoogle size={20} />
-							</i>
-						</div>
-						<div>
-							<Link
-								className="link1"
-								to="https://accounts.google.com/"
+		<>
+			{isLoggedin ? (
+				""
+			) : (
+				<div className="button">
+					<Popover>
+						<PopoverTrigger>
+							<Button
+								bg="none"
+								color="#fff"
+								fontWeight={500}
+								fontSize={15}
 							>
-								Google
-							</Link>
-						</div>
-						<i>
-							<BsArrowRight size={15} />
-						</i>
-					</div>
-				</PopoverContent>
-			</Popover>
-		</div>
+								Signin
+							</Button>
+						</PopoverTrigger>
+
+						<PopoverContent
+							className="layout"
+							w={"100%"}
+							p={15}
+							mt={15}
+							zIndex={1}
+						>
+							<PopoverArrow />
+							<div className="popover">
+								<PopoverHeader fontSize={25}>
+									Login / Create Account
+								</PopoverHeader>
+								<PopoverCloseButton
+									w={8}
+									border="none"
+									bg="none"
+								/>
+							</div>
+							<PopoverBody
+								textAlign={"left"}
+								w={280}
+								fontSize={15}
+								mt={6}
+								color="grey"
+							>
+								Register now and get 2000 Nykaa reward points
+								instantly!
+							</PopoverBody>
+							<div className="signupbutt">
+								<div>
+									<Link className="link1" to="/login">
+										Sign in with Mobile/Email{" "}
+									</Link>
+								</div>
+								<div>
+									<i>
+										<BsArrowRight size={15} />
+									</i>
+								</div>
+							</div>
+							<div>
+								<Link className="link1" to="/signup">
+									Sign Up{" "}
+								</Link>
+							</div>
+							<div className="google">
+								<div>
+									<i>
+										<FcGoogle size={20} />
+									</i>
+								</div>
+								<div>
+									<Link
+										className="link1"
+										to="https://accounts.google.com/"
+									>
+										Google
+									</Link>
+								</div>
+								<i>
+									<BsArrowRight size={15} />
+								</i>
+							</div>
+						</PopoverContent>
+					</Popover>
+				</div>
+			)}
+		</>
 	);
 };
 
